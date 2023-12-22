82c5ca0962e2d318b0d5e327ed35a46a83355ac0
Mohammad Anar <ai.anarul4232@gmail.com>
2023-12-22 19:38:16 +0600

    css slider complete

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'anarul': {
+          100: '#cffafe',
+          200: '#a5f3fc',
+          300: '#67e8f9',
+          light: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2',
+          700: '#0e7490',
+          800: '#155e75',
+          900: '#164e63',
+        },
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
